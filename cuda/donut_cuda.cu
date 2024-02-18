bbac6bfeb964c95865e878fb3e2949acae1d92fd
huxioan <hu7006@outlook.com>
2024-02-18 14:28:44 +0100

    copy to the kernel

--- cuda/donut_cuda.cu
+++ cuda/donut_cuda.cu
@@ -101,6 +101,62 @@ __host__ void renderFrame(float A, float B, char* outputP){ //A: rotate about x-
 
 __global__ void renderPointCuda(const float A, const float B, float* zBuf, char* output){
 
+    //get the theta and phi from index
+    float theta = blockIdx.x * thetaStep;
+    float phi = threadIdx.x * phiStep;
+
+    float sinTheta = sinf(theta);
+    float cosTheta = cosf(theta);
+
+    float sinPhi = sinf(phi);
+    float cosPhi = cosf(phi);
+
+    //common value
+    float sinA = sinf(A);
+    float cosA = cosf(A);
+    float sinB = sinf(B);
+    float cosB = cosf(B);
+
+
+    //without rotations, z = 0
+    float x = R2 + R1 * cosTheta;
+    float y = R1 * sinTheta;
+
+    //rotate matrix
+    float xRotatedo = x * (cosB * cosPhi + sinA * sinB * sinPhi) - y * cosA * sinB;
+    float yRotatedo = x * (sinB * cosPhi - sinA * cosB * sinPhi) + y * cosA * cosB;
+
+    float zRotated = K2 + cosA * x * sinPhi + y * sinA;
+
+    float ooz = 1 / zRotated;
+
+    int xP = (int) (resW / 2 + K1 * ooz * xRotatedo);
+    int yP = (int) (resH / 2 - K1 * 0.5 * ooz * yRotatedo);
+
+    if(xP >= resW || xP < 0 || yP >= resH || yP < 0){
+        //out of the screen, end thread for the point
+        return;
+    }
+
+    //illumination, Lvec * Normalvec
+    float lum = lX * (cosTheta * cosPhi * cosB + 
+                sinPhi * cosTheta * sinA * sinB - 
+                sinTheta * cosA * sinB) +
+                lY * (cosTheta * cosPhi * sinB +
+                cosB * sinTheta * cosA -
+                cosB * sinPhi * cosTheta * sinA) +
+                lZ * (sinA * sinTheta + sinPhi * cosTheta * cosA);
+
+    if( ooz > zBuf[xP + yP * resW] /* current point is closer to the viewer */&&
+        lum > 0 /* the point is visible */){
+        zBuf[xP + yP * resW] = ooz;
+
+        int lumIndex = lum * 11.3; //map the illuminance to the index
+        output[xP + resW * yP] = ".,-~:;=!*#$@"[lumIndex];
+    }
+
+
+
 }
 
 
@@ -133,7 +189,7 @@ __host__ int main(){
         cudaMemset(output, ' ', resW * resH * sizeof(char));
         cudaMemset(zBuf, 0, resW * resH * sizeof(float));
         
-        //thread for every point on the donut
+        //thread for every point on the donut, grid for small circle, block for big circle
         renderPointCuda<<<6.28/thetaStep,6.28/phiStep>>>(A, B, zBuf, output);
 
         cudaDeviceSynchronize();    
